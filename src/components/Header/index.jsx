97f4b0e68e618889b2c6496d273c5ea061409450
Raí Soares <raisoares.dev@gmail.com>
2023-08-02 01:36:25 -0300

    logout fixed

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,10 +2,17 @@ import { Container, Brand, Profile, ImgProfile } from "./styles";
 import { Input } from "../Input";
 import { useAuth } from "../../hooks/auth";
 import { api } from "../../services/api";
+import { useNavigate } from "react-router-dom";
 
 export function Header(){
+    const navigate = useNavigate();
     const {logout, user} = useAuth();
     const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
+
+    const handleLogout = () => {
+        navigate("/");
+        logout();
+    }
 
     return(
         <Container>
@@ -14,7 +21,7 @@ export function Header(){
             <Profile >
                 <div>
                     <strong>{user.name}</strong>
-                    <button onClick={logout}>Sair</button>
+                    <button onClick={handleLogout}>Sair</button>
                 </div>
             <ImgProfile to="/profile"><img src={avatarUrl} alt={user.name}/></ImgProfile>   
             </Profile>
