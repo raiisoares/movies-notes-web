754cb5a262d39022c6123922b21c429ec4bf173e
Raí Soares <raisoares.dev@gmail.com>
2023-08-02 02:42:16 -0300

    tag and note componentes refactored

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,9 +2,30 @@ import { AiOutlinePlus } from "react-icons/ai"
 import { Container, Content } from "./styles";
 import { Header } from "../../components/Header";
 import { Note } from "../../components/Note";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { api } from "../../services/api";
+
 
 export function Home(){
+    const navigate = useNavigate();
+    const [search, setSearch] = useState("");
+    const [notes, setNotes] = useState([]);
+   
+    const handlePreview = (id) => {
+        navigate(`/preview/${id}`);
+    }
+
+    useEffect(() => {
+        async function fetchNotes() {
+            const response = await api.get(`/notes?title=${search}`);
+            setNotes(response.data);
+        }
+
+        fetchNotes();
+
+    }, [search]);
+   
     return(
         <Container>
            <Header />
@@ -13,12 +34,8 @@ export function Home(){
                     <h1>Meus filmes</h1>
                     <Link to="/new"><AiOutlinePlus/> Adicionar filme</Link>
                 </div>
-                <Note to="/preview/2" title={"Interstellar"} />
-                <Note to="/preview/2" title={"Interstellar"} />
-                <Note to="/preview/2" title={"Interstellar"} />
-                <Note to="/preview/2" title={"Interstellar"} />
-                <Note to="/preview/2" title={"Interstellar"} />
-                <Note to="/preview/2" title={"Interstellar"} />
+                {notes.map(note => (<Note key={String(note.id)} data={note} onClick={() => handlePreview(note.id)} />))}
+
             </Content>
         </Container>
     );
--- src/pages/MoviePreview/index.jsx
+++ src/pages/MoviePreview/index.jsx
@@ -3,43 +3,55 @@ import { Header } from "../../components/Header"
 import { ButtonText } from "../../components/ButtonText"
 import { Star } from "../../components/Star";
 import { Tag } from "../../components/Tag";
-import { AiOutlineClockCircle, AiFillStar} from "react-icons/ai"
+import { AiOutlineClockCircle, AiFillStar } from "react-icons/ai"
+import { useState } from "react";
+import { api } from "../../services/api";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useAuth } from "../../hooks/auth";
 
-export function MoviePreview({ user, created_at }) {
-    return(
+
+export function MoviePreview() {
+    const params = useParams();
+    const [data, setData] = useState(null);
+    const navigate = useNavigate();
+    const { user } = useAuth();
+
+    useEffect(() => {
+        async function fetchNote() {
+            const response = await api.get(`/notes/${params.id}`);
+            console.log(response.data);
+            setData(response.data);
+        }
+        fetchNote();
+    }, []);
+
+    return (
         <Container>
-            <Header/>
+            <Header />
             <main>
-                <span><AiFillStar/></span>
-                <ButtonText to="/" value={"Voltar"}/>
-               
+                <span><AiFillStar /></span>
+                <ButtonText to="/" value={"Voltar"} />
+
                 <div>
-                    <h1>Interestellar</h1>
-                    <Star/>
-                </div>
-                
-                <div>                           
-                    <img src="https://www.github.com/raiisoares.png" alt="Foto do usuário"/>
-                    <p>por {user}</p>
-                    
-                    <p><AiOutlineClockCircle/> {created_at}</p> 
+                    <h1>{data.title}</h1>
+                    <Star />
                 </div>
 
                 <div>
-                    <Tag value={"Ficção Científica"}/>
-                    <Tag value={"Drama"}/>
-                    <Tag value={"Família"}/>
+                    <img src={user.avatar} alt={user.name} />
+                    <p>por {user.name}</p>
+
+                    <p><AiOutlineClockCircle /> { }</p>
                 </div>
-               
-                <p>Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de radiação gravitacional, deixando coordenadas em binário que os levam até uma instalação secreta da NASA liderada pelo professor John Brand. O cientista revela que um buraco de minhoca foi aberto perto de Saturno e que ele leva a planetas que podem oferecer condições de sobrevivência para a espécie humana. As "missões Lázaro" enviadas anos antes identificaram três planetas potencialmente habitáveis orbitando o buraco negro Gargântua: Miller, Edmunds e Mann – nomeados em homenagem aos astronautas que os pesquisaram. Brand recruta Cooper para pilotar a nave espacial Endurance e recuperar os dados dos astronautas; se um dos planetas se mostrar habitável, a humanidade irá seguir para ele na instalação da NASA, que é na realidade uma enorme estação espacial. A partida de Cooper devasta Murphy. <br /><br />
 
-                Além de Cooper, a tripulação da Endurance é formada pela bióloga Amelia, filha de Brand; o cientista Romilly, o físico planetário Doyle, além dos robôs TARS e CASE. Eles entram no buraco de minhoca e se dirigem a Miller, porém descobrem que o planeta possui enorme dilatação gravitacional temporal por estar tão perto de Gargântua: cada hora na superfície equivale a sete anos na Terra. Eles entram em Miller e descobrem que é inóspito já que é coberto por um oceano raso e agitado por ondas enormes. Uma onda atinge a tripulação enquanto Amelia tenta recuperar os dados de Miller, matando Doyle e atrasando a partida. Ao voltarem para a Endurance, Cooper e Amelia descobrem que 23 anos se passaram.
+                {data.tags &&
+                    <div>
+                        {data.tags.map(tag => (<Tag value={"Ficção Científica"} />))}
+                    </div>
+                }
 
-                Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de radiação gravitacional, deixando coordenadas em binário que os levam até uma instalação secreta da NASA liderada pelo professor John Brand. O cientista revela que um buraco de minhoca foi aberto perto de Saturno e que ele leva a planetas que podem oferecer condições de sobrevivência para a espécie humana. As "missões Lázaro" enviadas anos antes identificaram três planetas potencialmente habitáveis orbitando o buraco negro Gargântua: Miller, Edmunds e Mann – nomeados em homenagem aos astronautas que os pesquisaram. Brand recruta Cooper para pilotar a nave espacial Endurance e recuperar os dados dos astronautas; se um dos planetas se mostrar habitável, a humanidade irá seguir para ele na instalação da NASA, que é na realidade uma enorme estação espacial. A partida de Cooper devasta Murphy. <br /><br />
-                Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de radiação gravitacional, deixando coordenadas em binário que os levam até uma instalação secreta da NASA liderada pelo professor John Brand. O cientista revela que um buraco de minhoca foi aberto perto de Saturno e que ele leva a planetas que podem oferecer condições de sobrevivência para a espécie humana. As "missões Lázaro" enviadas anos antes identificaram três planetas potencialmente habitáveis orbitando o buraco negro Gargântua: Miller, Edmunds e Mann – nomeados em homenagem aos astronautas que os pesquisaram. Brand recruta Cooper para pilotar a nave espacial Endurance e recuperar os dados dos astronautas; se um dos planetas se mostrar habitável, a humanidade irá seguir para ele na instalação da NASA, que é na realidade uma enorme estação espacial. A partida de Cooper devasta Murphy. <br /><br />
-                Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de radiação gravitacional, deixando coordenadas em binário que os levam até uma instalação secreta da NASA liderada pelo professor John Brand. O cientista revela que um buraco de minhoca foi aberto perto de Saturno e que ele leva a planetas que podem oferecer condições de sobrevivência para a espécie humana. As "missões Lázaro" enviadas anos antes identificaram três planetas potencialmente habitáveis orbitando o buraco negro Gargântua: Miller, Edmunds e Mann – nomeados em homenagem aos astronautas que os pesquisaram. Brand recruta Cooper para pilotar a nave espacial Endurance e recuperar os dados dos astronautas; se um dos planetas se mostrar habitável, a humanidade irá seguir para ele na instalação da NASA, que é na realidade uma enorme estação espacial. A partida de Cooper devasta Murphy. <br /><br />
-                
-                </p>
+                <p>{data.description}</p>
             </main>
         </Container>
     );
